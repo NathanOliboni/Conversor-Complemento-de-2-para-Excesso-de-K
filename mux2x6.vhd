2fdd6899b52b0f2be0bee7a567727f9046b64ff0
Nathan Oliboni <116748360+NathanOliboni@users.noreply.github.com>
2022-12-12 22:03:13 -0300

    Update mux2x6.vhd
    Alteração para configuração correta do mux2x6

--- mux2x6.vhd
+++ mux2x6.vhd
@@ -4,10 +4,10 @@ use ieee.std_logic_1164.all;
         --multiplexador de 2 canais com 6 bits por canal 
 entity mux2x6 is
     port (
-        c0  : in std_logic_vector(5 downto 0);
-        c1  : in std_logic_vector(5 downto 0);
+        c0  : in std_logic_vector(4 downto 0);
+        c1  : in std_logic_vector(4 downto 0);
         sel : in std_logic;
-        z   : out std_logic_vector(5 downto 0)
+        z   : out std_logic_vector(4 downto 0)
     );
 end mux2x6;
 
@@ -15,6 +15,6 @@ architecture comuta of mux2x6 is
 begin
         --atribuição condicional
     z <= c0 when sel = '0' else
-    c1 when sel = '1' else "ZZZZZZ"; --Z = Alta impedância onde se assume qualquer valor
+    c1 when sel = '1' else (others => 'Z'); --Z = Alta impedância onde se assume qualquer valor
 
 end architecture;

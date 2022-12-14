1f7cb00e2ca5b1b03f4eac11437c47f021579dc1
Nathan Oliboni <116748360+NathanOliboni@users.noreply.github.com>
2022-12-13 22:39:26 -0300

    Update tb_trabalho.vhdl
    Alteração para correção do tb.

--- tb_trabalho.vhdl
+++ tb_trabalho.vhdl
@@ -26,9 +26,9 @@ end component;
 
   component conversor is
     port (
-        conv_in : std_logic_vector(4 downto 0);
-        conv_out: std_logic_vector(4 downto 0);
-        error_out:std_logic
+        conv_in : in std_logic_vector(4 downto 0);
+        conv_out: out std_logic_vector(4 downto 0);
+        error_out: out std_logic
     );
 end component;
 
@@ -36,14 +36,16 @@ signal sc0, sc1, ss0, ss1, sz: std_logic_vector(4 downto 0);
 signal ssel, serror_out : std_logic;
 
 begin
-  u_mux2x6    : mux2x6 port map (sc0, sc1, ssel, sz);
-  u_demux2x6  : demux2x6 port map (ss0, ss1, ssel, sz);
+  u_mux2x6 : mux2x6 port map (sc0, sc1, ssel, sz);
+  u_demux2x6 : demux2x6 port map (ss0, ss1, ssel, sz);
   u_conversor : conversor port map (sc0, sc1, serror_out);
   
   u_teste : process
     begin
 
     sc0 <= "00000";
+    
+    ssel<= '0';
     wait for 10 ns;
     
     sc0 <= "00001";
@@ -142,8 +144,6 @@ begin
     ssel<= '1';
     wait for 10 ns;
 
-    ssel<= '0';
-    wait for 10 ns;
     wait;
     end process;
 
